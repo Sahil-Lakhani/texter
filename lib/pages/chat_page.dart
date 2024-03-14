206755e3520a4e8f70a1ecd574a8c9d1265dbebd
Sahil-Lakhani <sahillakhani266@gmail.com>
2024-03-14 17:06:08 +0530

    chat page updated

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,23 +1,113 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:texter/components/my_text_field.dart';
+import 'package:texter/services/chat/chat_service.dart';
 
 class ChatPage extends StatefulWidget {
   final String receiverUserEmail;
   final String receiverUserID;
-  const ChatPage({super.key,
-  required this.receiverUserEmail,
-  required this.receiverUserID});
+  const ChatPage(
+      {super.key,
+      required this.receiverUserEmail,
+      required this.receiverUserID});
 
   @override
   State<ChatPage> createState() => _ChatPageState();
 }
 
 class _ChatPageState extends State<ChatPage> {
+  final TextEditingController _messageController = TextEditingController();
+  final ChatService _chatService = ChatService();
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  void sendMessage() async {
+    // Send message if there's something to send in the text box (input)
+    if (_messageController.text.isNotEmpty) {
+      await _chatService.sendMessage(
+          widget.receiverUserID, _messageController.text);
+      // Clear the text field after sending the message
+      _messageController.clear();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.receiverUserEmail),
       ),
+      body: Column(
+        children: [
+          Expanded(
+            child: _buildingMessageList(),
+          ),
+          //user input
+          _buildMessageInput(),
+        ],
+      ),
+    );
+  }
+
+  //build message list?
+  Widget _buildingMessageList() {
+    return StreamBuilder(
+      stream: _chatService.getMessages(
+          widget.receiverUserID, _firebaseAuth.currentUser!.uid),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Text('Error ${snapshot.error}');
+        }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text('Loading..');
+        }
+        return ListView(
+          children: snapshot.data!.docs
+              .map((document) => _buildMessageItem(document))
+              .toList(),
+        );
+      },
+    );
+  }
+
+//message Item
+  Widget _buildMessageItem(DocumentSnapshot document) {
+    Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+
+    var alignment = (data['senderId'] == _firebaseAuth.currentUser!.uid)
+        ? Alignment.centerRight
+        : Alignment.centerLeft;
+
+    return Container(
+      alignment: alignment,
+      child: Column(
+        children: [
+          Text(data['senderEmail']),
+          Text(data['message']),
+        ],
+      ),
+    );
+  }
+
+//the message input item (format)
+  Widget _buildMessageInput() {
+    return Row(
+      children: [
+        Expanded(
+          child: MyTextField(
+            controller: _messageController,
+            hintText: 'Enter Message',
+            obscureText: false,
+          ),
+        ),
+        IconButton(
+          onPressed: sendMessage,
+          icon: const Icon(
+            Icons.send,
+            size: 40,
+          ),
+        ),
+      ],
     );
   }
 }
